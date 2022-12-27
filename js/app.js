97f0685ee710a2422798d09421cebd6e8b014d19
dkostmii <58741422+dkostmii@users.noreply.github.com>
2022-12-27 18:59:15 +0000

    Deploying to gh-pages from @ dkostmii/chaingex@f98b6bc01e4ed180866b57f0ad0ae3262eadc0da 🚀

--- js/app.js
+++ js/app.js
@@ -4946,18 +4946,44 @@
     }
     (() => {
         "use strict";
-        let valueDisplays = document.querySelectorAll(".num");
-        let interval = 2e3;
-        valueDisplays.forEach((valueDisplay => {
-            let startValue = 0;
-            let endValue = parseInt(valueDisplay.getAttribute("data-val"));
-            let duration = Math.floor(interval / endValue);
-            let counter = setInterval((function() {
-                startValue += 1;
-                valueDisplay.textContent = startValue;
-                if (startValue == endValue) clearInterval(counter);
-            }), duration);
-        }));
+        function getAnimationStartScrollPosition(el) {
+            const {innerHeight: windowHeight} = window;
+            const {top} = el.getBoundingClientRect();
+            return top + windowHeight / 2;
+        }
+        function animate(onAnimationStarted) {
+            const animatedElements = document.querySelectorAll(".animation-on-scroll");
+            const animationTimeMs = 4e3;
+            let onAnimationStartedCalled = false;
+            animatedElements.forEach((el => {
+                const start = 0, end = parseInt(el.dataset.val);
+                const timeoutDuration = animationTimeMs / end;
+                let value = start;
+                const incrementValue = () => {
+                    value += 1;
+                    const prevValue = parseInt("0" | el.textContent);
+                    if (value > prevValue) el.textContent = value;
+                };
+                const interval = setInterval((() => {
+                    if (!onAnimationStartedCalled && onAnimationStarted instanceof Function) {
+                        onAnimationStartedCalled = true;
+                        onAnimationStarted();
+                    }
+                    if (value >= end) {
+                        clearInterval(interval);
+                        return;
+                    }
+                    incrementValue();
+                }), timeoutDuration);
+            }));
+        }
+        function scrollHandler() {
+            const {scrollY} = window;
+            const triggerElement = document.querySelector(".features__content");
+            if (null !== triggerElement) if (scrollY >= getAnimationStartScrollPosition(triggerElement)) animate((() => window.removeEventListener("scroll", scrollHandler)));
+        }
+        window.addEventListener("scroll", scrollHandler);
+        scrollHandler();
         function isWebp() {
             function testWebP(callback) {
                 let webP = new Image;
