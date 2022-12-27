57be3fcaedc029f2e0a87788417a71e361451271
WebBobuskiy <113801059+WebBobuskiy@users.noreply.github.com>
2022-12-27 10:06:03 +0200

    Start animation when scrolling to the block

--- src/js/num/num.js
+++ src/js/num/num.js
@@ -1,16 +1,35 @@
+`use strict`;
+class Options {
+   constructor(animationTime, pixelsFromBottom = 0, reverseFade = false) {
+      this.pixelsFromBottom = -pixelsFromBottom;
+      this.animationTime = animationTime;
+      this.reverseFade = reverseFade;
+   }
+}
 
-let valueDisplays = document.querySelectorAll(".num");
-let interval = 2000;
+//Numbers
+let scrolled = window.pageYOffset;
+let windowHeight = window.innerHeight;
 
-valueDisplays.forEach((valueDisplay) => {
-   let startValue = 0;
-   let endValue = parseInt(valueDisplay.getAttribute("data-val"));
-   let duration = Math.floor(interval / endValue);
-   let counter = setInterval(function () {
-      startValue += 1;
-      valueDisplay.textContent = startValue;
-      if (startValue == endValue) {
-         clearInterval(counter);
-      }
-   }, duration);
-});
+function rectObj(el) {
+   let rect = el.getBoundingClientRect();
+   return rect.top - windowHeight;
+}
+
+window.onscroll = function () {
+   let valueDisplays = document.querySelectorAll(".num");
+   let interval = 4000;
+   valueDisplays.forEach((valueDisplay) => {
+      let startValue = 0;
+      let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+      let duration = Math.floor(interval / endValue);
+      let counter = setInterval(function () {
+         startValue += 1;
+         valueDisplay.textContent = startValue;
+         if (startValue == endValue) {
+            clearInterval(counter);
+         }
+      }, duration);
+   });
+}
+
