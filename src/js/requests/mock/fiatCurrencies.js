e9544653697742ff8af3de19da41997fb8ed79e6
Dmytro-Andrii Kostelnyi <dmitrokostelny95@gmail.com>
2023-02-05 12:28:08 +0200

    Update fiat currencies mock

--- src/js/requests/mock/fiatCurrencies.js
+++ src/js/requests/mock/fiatCurrencies.js
@@ -1,7 +1,6 @@
 import { Currency } from "../../types/currency.js";
+import { currencies } from "../../config/currencies.js";
 
-const fiatCurrencies = [
-  { id: "usd", name: "US Dollar", short: "USD", price: 1, change: -0.01, card: "4444444444444444" },
-].map(c => new Currency(c));
+const fiatCurrenciesMock = currencies.map(c => new Currency({ ...c, price: 1, change: -0.01 }));
 
-export default fiatCurrencies;
+export default fiatCurrenciesMock;
